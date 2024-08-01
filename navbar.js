02fd990303a293e1c457da260b1f681d382645fd
the-dg04 <devgup04@gmail.com>
2024-08-01 05:54:55 +0530

    dcdscds

--- navbar.js
+++ navbar.js
@@ -6,8 +6,6 @@ window.onload=()=>{
 
 document.getElementsByClassName("hamburger-menu")[0].onclick=()=>{
     nav_menu=document.getElementsByClassName("navlinks")[0];
-    console.log(nav_menu);
-    console.log(closed);
     if(closed){
         nav_menu.style.display="flex";
     }else{

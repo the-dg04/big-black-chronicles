9648ba405dbe00c9353d69f38bf7fc94e5bda299
the-dg04 <devgup04@gmail.com>
2024-08-01 15:29:05 +0530

    gfdbfdvfvfsv

--- navbar.js
+++ navbar.js
@@ -4,6 +4,14 @@ window.onload=()=>{
     closed=true;
 }
 
+window.onresize=()=>{
+    if(window.innerWidth>=600){
+        document.getElementsByClassName("navlinks")[0].style.display="flex";
+    }else{
+        document.getElementsByClassName("navlinks")[0].style.display="none";
+    }
+}
+
 document.getElementsByClassName("hamburger-menu")[0].onclick=()=>{
     nav_menu=document.getElementsByClassName("navlinks")[0];
     if(closed){
